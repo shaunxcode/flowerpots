711707fd5ed720535c860c7e6fa540626a0c9619
shaun gilchrist <shaun.gilchrist@venafi.com>
2013-04-26 14:39:58 -0600

    adding code browser example

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 lessc src/example.less > example/example.css
 coffee -o ./example -c src/example.coffee 
+coffee -o ./example -c src/codeBrowser.coffee
 lessc src/index.less > index.css
 coffee -o ./ -c src/index.coffee
 component install
